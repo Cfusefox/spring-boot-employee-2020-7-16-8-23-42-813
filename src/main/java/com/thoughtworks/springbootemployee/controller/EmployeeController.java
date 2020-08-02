ea957be41133f7721c187471aedabace75c2f973
fox <1036674617@qq.com>
2020-08-02 10:42:50 +0800

    [York] feature: add getAllEmployees

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,10 +1,30 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+
+
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(0, "Xiaoming", 20, "Male"));
+        employees.add(new Employee(1, "Xiaohong", 19, "Female"));
+        employees.add(new Employee(2, "Xiaozhi",  15, "Male"));
+        employees.add(new Employee(3, "Xiaogang", 16, "Male"));
+        employees.add(new Employee(4, "Xiaoxia",  15, "Female"));
+        return employees;
+    }
+
 
 }
